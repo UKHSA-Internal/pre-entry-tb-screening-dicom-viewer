b29b5e4bda40cb07cb317e7873d5e6fe0724fbb5
ogl99 <oghosa.igiehon@ukhsa.gov.uk>
2025-05-22 15:00:26 +0100

    terraform fmt and lint

--- modules/oidc/oidc.tf
+++ modules/oidc/oidc.tf
@@ -1,4 +1,4 @@
-data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
 
 # module "iam_github_oidc_provider" {
 #   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
--- qa-services/oidc/main.tf
+++ qa-services/oidc/main.tf
@@ -3,7 +3,7 @@ module "oidc" {
   #checkov:skip=CKV_TF_1:UKHSA "Internal module, release process to be defined"
   #checkov:skip=CKV_TF_2:UKHSA "Internal module, release process to be defined"
   iac_iam         = var.iac_iam
+  environment_iac = var.environment_iac
   # app_iam         = var.app_iam
   # environment_app = var.environment_app
-  environment_iac = var.environment_iac
 }
