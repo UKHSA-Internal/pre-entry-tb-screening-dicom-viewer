50e4697af6721a6dae6c8b774bf6f5043e37554f
ogl99 <oghosa.igiehon@ukhsa.gov.uk>
2025-05-22 17:19:15 +0100

    provider

--- modules/qa-oidc/oidc.tf
+++ modules/qa-oidc/oidc.tf
@@ -1,9 +1,9 @@
 # data "aws_caller_identity" "current" {}
 
-# module "iam_github_oidc_provider" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
-#   version = "~> 5.54.0"
-# }
+module "iam_github_oidc_provider" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
+  version = "~> 5.54.0"
+}
 
 resource "aws_iam_policy" "iac" {
   name = "iac-qa-github-actions-oidc"
